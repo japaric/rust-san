347f8e0e278c2f36a98f0d121399185e034867b1
gnzlbg <gonzalobg88@gmail.com>
2018-02-01 16:46:50 +0100

    make the data-race false-positive in rust test surface

--- tsan/src/lib.rs
+++ tsan/src/lib.rs
@@ -20,6 +20,40 @@ mod tests {
         foo();
     }
 
+    #[test]
+    fn data_race_fixed1() {
+        use std::thread;
+        use std::sync::atomic::{AtomicI32, Ordering};
+
+        static ANSWER: AtomicI32 = AtomicI32::new(0);
+
+        fn foo() {
+            let t1 = thread::spawn(|| ANSWER.store(42, Ordering::SeqCst) );
+            ANSWER.store(24, Ordering::SeqCst);
+            t1.join().ok();
+            let v = ANSWER.load(Ordering::SeqCst);
+            assert!(v == 42 || v == 24);
+        }
+        foo();
+    }
+
+    #[test]
+    fn data_race_fixed2() {
+        use std::thread;
+        use std::sync::atomic::{AtomicI32, Ordering};
+
+        static ANSWER: AtomicI32 = AtomicI32::new(0);
+
+        fn foo() {
+            let t1 = thread::spawn(|| ANSWER.store(42, Ordering::SeqCst) );
+            ANSWER.store(24, Ordering::SeqCst);
+            t1.join().ok();
+            let v = ANSWER.load(Ordering::SeqCst);
+            assert!(v == 42 || v == 24);
+        }
+        foo();
+    }
+
     #[cfg(feature = "fail")]
     #[test]
     fn data_race() {
